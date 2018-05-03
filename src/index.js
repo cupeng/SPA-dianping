d9a09cab0ca3b3756ad4ce0d26f7c97d24b3a523
cupeng <872345732@qq.com>
2018-05-03 20:55:40 +0800

    dev init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-class App extends React.Component {
-    render() {
-        return <h1>hello1 react</h1>
-    }
-}
-
 render(
     <App />,
     document.querySelector('#app')
