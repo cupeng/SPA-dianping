593e0f5f3bcbdab58260f1192017c83e913df684
cupeng <872345732@qq.com>
2018-05-09 21:25:16 +0800

    project init

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './index.scss'
+
+class Header extends React.Component {
+    render () {
+        return (
+            <div className="header-wrap">
+                <i className="fa fa-free-code-camp"></i>
+            </div>
+        )
+    }
+}
+
+export default Header
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require("path")
-
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const isDev = process.env.NODE_ENV === 'development'
 module.exports = {
+    mode: isDev ? 'development' : 'production',
+	entry: './src/index.js',
+	output: {
+		filename: 'js/bundle.[hash:8].js',
+        path: path.resolve(__dirname,'dist'),
+        publicPath: '/dist/'
+	},
     module: {
         rules: [
             {
-                test: /\.(js|jsx)/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
+				include: /src/,
                 use: {
                     loader: 'babel-loader',
                     options: {
@@ -14,25 +25,78 @@ module.exports = {
                 }
             },
             {
-                test: /\.(png|jpg|gif)/,
+                test: /\.(png|jpg|gif)$/,
                 use: [
                     {
-                        loader: "file-loader",
-                        options: {}
+                      loader: 'url-loader',
+                      options: {
+                        limit: 8192,
+                        name: 'resource/images/[name].[ext]',
+                        publicPath: '../'
+                      }
+                    }
+                ]
+            },
+			{
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+                use: [
+                    {
+                      loader: 'url-loader',
+                      options: {
+                        limit: 8192,
+                        name: 'resource/fonts/[name].[ext]',
+                        publicPath: '../'
+                      }
                     }
                 ]
             },
             {
                 test: /\.css$/,
-                use: [
-                    {loader: "style-loader"},
-                    {loader: "css-loader?modules"},
-                ]
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader","postcss-loader"]
+
+                })
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader","sass-loader","postcss-loader"]
+
+                })
             }
         ]
     },
+	resolve : {
+        alias : {
+            node_modules: __dirname + '/node_modules'
+        },
+        extensions: ['.js','.jsx','scss']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            title: "React App",
+            favicon: "./favicon.ico",
+			minify: {
+				collapseWhitespace: true
+            }
+			
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/bundle.[hash:8].css',
+            disable: isDev
+        }),
+		new webpack.HotModuleReplacementPlugin()
+
+    ],
     devServer: {
-        inline: true,
-        port: 8080
+        hot:true,
+        compress: true,
+        port: 8080,
+        historyApiFallback: {
+            index: "/dist/index.html"
+        }
     }
 }
